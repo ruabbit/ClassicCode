a2361754fca6fee45935a99ef5b65a689d84d540
ruabbit <github@tanmy.dev>
2026-05-11 04:06:58 +0800

    Render markdown in transcript bubbles

--- Sources/iOS/CCWorkbenchDetailViewController.m
+++ Sources/iOS/CCWorkbenchDetailViewController.m
@@ -3,6 +3,7 @@
 #import "CCLineRemoteControlAdapter.h"
 #import "CCRemoteControl.h"
 #import <QuartzCore/QuartzCore.h>
+#include <math.h>
 
 @interface CCWorkbenchDetailViewController () <UITextFieldDelegate>
 @end
@@ -136,6 +137,229 @@
     return @"";
 }
 
+- (NSDictionary *)attributesWithFont:(UIFont *)font color:(UIColor *)color
+{
+    NSMutableParagraphStyle *style = [[[NSMutableParagraphStyle alloc] init] autorelease];
+    style.lineBreakMode = NSLineBreakByWordWrapping;
+    style.paragraphSpacing = 4.0;
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            font, NSFontAttributeName,
+            color, NSForegroundColorAttributeName,
+            style, NSParagraphStyleAttributeName,
+            nil];
+}
+
+- (void)appendString:(NSString *)string
+                font:(UIFont *)font
+               color:(UIColor *)color
+    toAttributedString:(NSMutableAttributedString *)attributed
+{
+    if ([string length] == 0) {
+        return;
+    }
+    NSAttributedString *piece = [[[NSAttributedString alloc] initWithString:string
+                                                                 attributes:[self attributesWithFont:font color:color]] autorelease];
+    [attributed appendAttributedString:piece];
+}
+
+- (NSRange)rangeOfString:(NSString *)needle inString:(NSString *)haystack fromIndex:(NSUInteger)index
+{
+    if (index >= [haystack length]) {
+        return NSMakeRange(NSNotFound, 0);
+    }
+    return [haystack rangeOfString:needle
+                           options:0
+                             range:NSMakeRange(index, [haystack length] - index)];
+}
+
+- (void)appendInlineMarkdown:(NSString *)text
+                       font:(UIFont *)font
+                      color:(UIColor *)color
+         toAttributedString:(NSMutableAttributedString *)attributed
+{
+    NSUInteger index = 0;
+    while (index < [text length]) {
+        NSRange codeRange = [self rangeOfString:@"`" inString:text fromIndex:index];
+        NSRange boldRange = [self rangeOfString:@"**" inString:text fromIndex:index];
+        NSRange italicRange = [self rangeOfString:@"*" inString:text fromIndex:index];
+
+        NSRange tokenRange = NSMakeRange(NSNotFound, 0);
+        NSString *token = nil;
+        UIFont *tokenFont = nil;
+        if (codeRange.location != NSNotFound) {
+            tokenRange = codeRange;
+            token = @"`";
+            tokenFont = [UIFont fontWithName:@"Courier" size:font.pointSize - 1.0];
+        }
+        if (boldRange.location != NSNotFound &&
+            (tokenRange.location == NSNotFound || boldRange.location < tokenRange.location)) {
+            tokenRange = boldRange;
+            token = @"**";
+            tokenFont = [UIFont boldSystemFontOfSize:font.pointSize];
+        }
+        if (italicRange.location != NSNotFound &&
+            italicRange.location + 1 < [text length] &&
+            ![[text substringWithRange:NSMakeRange(italicRange.location, 2)] isEqualToString:@"**"] &&
+            (tokenRange.location == NSNotFound || italicRange.location < tokenRange.location)) {
+            tokenRange = italicRange;
+            token = @"*";
+            tokenFont = [UIFont italicSystemFontOfSize:font.pointSize];
+        }
+
+        if (tokenRange.location == NSNotFound) {
+            [self appendString:[text substringFromIndex:index] font:font color:color toAttributedString:attributed];
+            break;
+        }
+
+        if (tokenRange.location > index) {
+            [self appendString:[text substringWithRange:NSMakeRange(index, tokenRange.location - index)]
+                          font:font
+                         color:color
+            toAttributedString:attributed];
+        }
+
+        NSUInteger contentStart = tokenRange.location + [token length];
+        NSRange closeRange = [self rangeOfString:token inString:text fromIndex:contentStart];
+        if (closeRange.location == NSNotFound) {
+            [self appendString:[text substringFromIndex:tokenRange.location] font:font color:color toAttributedString:attributed];
+            break;
+        }
+
+        NSString *content = [text substringWithRange:NSMakeRange(contentStart, closeRange.location - contentStart)];
+        [self appendString:content
+                      font:(tokenFont != nil ? tokenFont : font)
+                     color:([token isEqualToString:@"`"] ? [UIColor colorWithWhite:0.20 alpha:1.0] : color)
+        toAttributedString:attributed];
+        index = closeRange.location + [token length];
+    }
+}
+
+- (NSString *)trimmedMarkdownLine:(NSString *)line
+{
+    return [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+}
+
+- (NSString *)listPrefixForLine:(NSString *)line contentStart:(NSUInteger *)contentStart
+{
+    if ([line length] >= 2) {
+        NSString *prefix = [line substringToIndex:2];
+        if ([prefix isEqualToString:@"- "] || [prefix isEqualToString:@"* "] || [prefix isEqualToString:@"+ "]) {
+            if (contentStart != NULL) {
+                *contentStart = 2;
+            }
+            return @"- ";
+        }
+    }
+
+    NSUInteger index = 0;
+    while (index < [line length]) {
+        unichar ch = [line characterAtIndex:index];
+        if (ch < '0' || ch > '9') {
+            break;
+        }
+        index++;
+    }
+    if (index > 0 && index + 1 < [line length] &&
+        [line characterAtIndex:index] == '.' &&
+        [line characterAtIndex:index + 1] == ' ') {
+        if (contentStart != NULL) {
+            *contentStart = index + 2;
+        }
+        return [line substringToIndex:index + 2];
+    }
+    return nil;
+}
+
+- (NSAttributedString *)attributedBodyForText:(NSString *)text role:(NSString *)role
+{
+    UIFont *baseFont = [role isEqualToString:@"tool"] ? [UIFont fontWithName:@"Courier" size:12.0] : [UIFont systemFontOfSize:14.0];
+    UIColor *baseColor = [UIColor blackColor];
+    NSMutableAttributedString *attributed = [[[NSMutableAttributedString alloc] init] autorelease];
+
+    if ([role isEqualToString:@"tool"]) {
+        [self appendString:text font:baseFont color:baseColor toAttributedString:attributed];
+        return attributed;
+    }
+
+    NSArray *lines = [text componentsSeparatedByString:@"\n"];
+    BOOL inCodeBlock = NO;
+    for (NSString *rawLine in lines) {
+        NSString *line = rawLine;
+        NSString *trimmed = [self trimmedMarkdownLine:line];
+        if ([trimmed hasPrefix:@"```"]) {
+            inCodeBlock = !inCodeBlock;
+            continue;
+        }
+
+        if (inCodeBlock) {
+            [self appendString:line
+                          font:[UIFont fontWithName:@"Courier" size:12.0]
+                         color:[UIColor colorWithWhite:0.15 alpha:1.0]
+            toAttributedString:attributed];
+            [self appendString:@"\n" font:baseFont color:baseColor toAttributedString:attributed];
+            continue;
+        }
+
+        NSUInteger headingLevel = 0;
+        while (headingLevel < [trimmed length] && headingLevel < 3 &&
+               [trimmed characterAtIndex:headingLevel] == '#') {
+            headingLevel++;
+        }
+        if (headingLevel > 0 && headingLevel + 1 < [trimmed length] &&
+            [trimmed characterAtIndex:headingLevel] == ' ') {
+            CGFloat size = (headingLevel == 1) ? 18.0 : (headingLevel == 2 ? 16.0 : 15.0);
+            [self appendString:[trimmed substringFromIndex:headingLevel + 1]
+                          font:[UIFont boldSystemFontOfSize:size]
+                         color:baseColor
+            toAttributedString:attributed];
+            [self appendString:@"\n" font:baseFont color:baseColor toAttributedString:attributed];
+            continue;
+        }
+
+        if ([trimmed hasPrefix:@">"]) {
+            NSString *quote = [[trimmed substringFromIndex:1] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            [self appendString:@"| "
+                          font:[UIFont italicSystemFontOfSize:baseFont.pointSize]
+                         color:[UIColor darkGrayColor]
+            toAttributedString:attributed];
+            [self appendInlineMarkdown:quote
+                                  font:[UIFont italicSystemFontOfSize:baseFont.pointSize]
+                                 color:[UIColor darkGrayColor]
+                    toAttributedString:attributed];
+            [self appendString:@"\n" font:baseFont color:baseColor toAttributedString:attributed];
+            continue;
+        }
+
+        NSUInteger contentStart = 0;
+        NSString *listPrefix = [self listPrefixForLine:trimmed contentStart:&contentStart];
+        if (listPrefix != nil) {
+            [self appendString:listPrefix font:baseFont color:baseColor toAttributedString:attributed];
+            [self appendInlineMarkdown:[trimmed substringFromIndex:contentStart]
+                                  font:baseFont
+                                 color:baseColor
+                    toAttributedString:attributed];
+            [self appendString:@"\n" font:baseFont color:baseColor toAttributedString:attributed];
+            continue;
+        }
+
+        [self appendInlineMarkdown:line font:baseFont color:baseColor toAttributedString:attributed];
+        [self appendString:@"\n" font:baseFont color:baseColor toAttributedString:attributed];
+    }
+
+    return attributed;
+}
+
+- (CGSize)sizeForAttributedString:(NSAttributedString *)attributed width:(CGFloat)width fallbackFont:(UIFont *)font
+{
+    if ([attributed length] == 0) {
+        return CGSizeMake(0.0, font.lineHeight);
+    }
+    CGRect rect = [attributed boundingRectWithSize:CGSizeMake(width, 10000.0)
+                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                           context:nil];
+    return CGSizeMake(ceilf(rect.size.width), ceilf(rect.size.height));
+}
+
 - (void)clearMessageViews
 {
     NSArray *subviews = [[_messageScrollView subviews] copy];
@@ -172,8 +396,9 @@
 
         UIFont *titleFont = [UIFont boldSystemFontOfSize:11.0];
         UIFont *bodyFont = [role isEqualToString:@"tool"] ? [UIFont fontWithName:@"Courier" size:12.0] : [UIFont systemFontOfSize:14.0];
+        NSAttributedString *bodyAttributed = [self attributedBodyForText:text role:role];
         CGSize titleSize = [title sizeWithFont:titleFont constrainedToSize:CGSizeMake(maxBubbleWidth - labelInset * 2.0, 18.0)];
-        CGSize bodySize = [text sizeWithFont:bodyFont constrainedToSize:CGSizeMake(maxBubbleWidth - labelInset * 2.0, 10000.0) lineBreakMode:NSLineBreakByWordWrapping];
+        CGSize bodySize = [self sizeForAttributedString:bodyAttributed width:maxBubbleWidth - labelInset * 2.0 fallbackFont:bodyFont];
         CGFloat bubbleWidth = MAX(titleSize.width, bodySize.width) + labelInset * 2.0;
         bubbleWidth = MIN(maxBubbleWidth, MAX(96.0, bubbleWidth));
         CGFloat bubbleHeight = titleSize.height + bodySize.height + 18.0;
@@ -198,7 +423,11 @@
         bodyLabel.font = bodyFont;
         bodyLabel.numberOfLines = 0;
         bodyLabel.lineBreakMode = NSLineBreakByWordWrapping;
-        bodyLabel.text = text;
+        if ([bodyLabel respondsToSelector:@selector(setAttributedText:)]) {
+            bodyLabel.attributedText = bodyAttributed;
+        } else {
+            bodyLabel.text = [bodyAttributed string];
+        }
         [bubble addSubview:bodyLabel];
 
         y += bubbleHeight + margin;
