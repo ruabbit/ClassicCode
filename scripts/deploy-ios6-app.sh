183a09db69079ba113040562c8022fd0d4187754
ruabbit <github@tanmy.dev>
2026-05-10 23:17:47 +0800

    Use remote git checkout for 10.9 builds

--- scripts/deploy-ios6-app.sh
+++ scripts/deploy-ios6-app.sh
@@ -4,12 +4,27 @@ set -eu
 BUILD_HOST=${BUILD_HOST:-classiccode-mac109}
 REMOTE_DIR=${REMOTE_DIR:-ClassicCode}
 IOS_DEVICE_HOST=${IOS_DEVICE_HOST:-classiccode-ipad6-via-local}
+REMOTE_BRANCH=${REMOTE_BRANCH:-main}
+REPO_URL=${REPO_URL:-https://github.com/ruabbit/ClassicCode.git}
+REMOTE_GIT_SSL_NO_VERIFY=${REMOTE_GIT_SSL_NO_VERIFY:-1}
 
 cd "$(dirname "$0")/.."
 
-rsync -az --delete \
-  --exclude .git \
-  --exclude build \
-  ./ "$BUILD_HOST:$REMOTE_DIR/"
-
-ssh "$BUILD_HOST" "cd '$REMOTE_DIR' && make deploy-ios IOS_DEVICE_HOST='$IOS_DEVICE_HOST'"
+ssh "$BUILD_HOST" "
+set -eu
+if [ '$REMOTE_GIT_SSL_NO_VERIFY' = '1' ]; then
+  export GIT_SSL_NO_VERIFY=true
+fi
+if [ ! -d '$REMOTE_DIR/.git' ]; then
+  if [ -e '$REMOTE_DIR' ]; then
+    mv '$REMOTE_DIR' '$REMOTE_DIR.rsync-backup-'\"\$(date +%Y%m%d%H%M%S)\"
+  fi
+  git clone -b '$REMOTE_BRANCH' '$REPO_URL' '$REMOTE_DIR'
+fi
+cd '$REMOTE_DIR'
+git remote set-url origin '$REPO_URL'
+git fetch origin '$REMOTE_BRANCH'
+git checkout '$REMOTE_BRANCH' >/dev/null 2>&1 || git checkout -b '$REMOTE_BRANCH' 'origin/$REMOTE_BRANCH'
+git reset --hard 'origin/$REMOTE_BRANCH'
+make deploy-ios IOS_DEVICE_HOST='$IOS_DEVICE_HOST'
+"
